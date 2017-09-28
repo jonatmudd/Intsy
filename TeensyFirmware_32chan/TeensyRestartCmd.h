eec464e40cf95adc8ffd130f69329f1e7eff449e
jonatmudd <jonathan.c.erickson@gmail.com>
2017-09-28 17:02:45 -0400

    Add files via upload

---
+++ TeensyFirmware_32chan/TeensyRestartCmd.h
@@ -0,0 +1,11 @@
+/* For software reset. This is Teensy 3.1 specific.
+  See: https://forums.adafruit.com/viewtopic.php?f=24&t=74769 and
+  https://forum.pjrc.com/threads/24304-_reboot_Teensyduino()-vs-_restart_Teensyduino()
+*/
+extern void softRestart();
+
+#define CPU_RESTART_ADDR (uint32_t *)0xE000ED0C
+#define CPU_RESTART_VAL 0x5FA0004
+#define CPU_RESTART (*CPU_RESTART_ADDR = CPU_RESTART_VAL);
+#define SCB_AIRCR (*(volatile uint32_t *)0xE000ED0C) // Application Interrupt and Reset Control location
+
