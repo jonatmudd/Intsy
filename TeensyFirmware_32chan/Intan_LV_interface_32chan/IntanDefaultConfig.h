ec1193193f80f3096ce0ae7394c0380068291ae5
jonatmudd <jonathan.c.erickson@gmail.com>
2017-09-28 16:46:58 -0400

    Add files via upload

---
+++ TeensyFirmware_32chan/Intan_LV_interface_32chan/IntanDefaultConfig.h
@@ -0,0 +1,59 @@
+// -----------------------------------------------------------------------------
+// DEFINE DEFAULT VALUES FOR WRITING INTAN RHD2132 REGISTERS
+// These define various modes of operation with the chip, such as abs mode
+// Note that WEAK_MISO can be set 0 if expanding to 64 channel system
+// ** indicates user selectable
+// -----------------------------------------------------------------------------
+
+// extern function declaration
+extern void ConfigureRegisters_0_17();
+
+
+// REG 0
+const byte ADC_REF_BW = 3; // always set to 3 (see RHD2000 datasheet, page 19)
+byte AMP_fast_settle = 0; //Set to 1 to enable  **
+byte AMP_Vref_En = 1; // wait 100 us before ADC calibration; set to 0 to turn off this feature **
+const byte ADC_comparator_bias = 3; // always set to 3 for normal operation
+const byte ADC_comparator_select = 2; // always set to 2 for normal operation
+
+// REG 1
+byte ADC_BUFFER_BIAS = 32; // optimum current value. See page 27 **
+byte VDD_SENSE_EN = 0; // set to 0 for no voltage supply sensor  **
+
+// REG 2
+const byte MUX_BIAS = 40; // set to 40 for <120kS/s
+
+// REG 3
+const byte MUX_LOAD = 0; // always set to 0 for normal operation (page 20)
+byte TEMP_S1 = 0; // temperature sense enable   **
+byte TEMP_S2 = 0; // temperature sense enable   **
+byte TEMP_EN = 0; // set to 0 to disable and save power ** 
+byte DIGOUT_HiZ = 1; // set to 1 for high impedance mode. **
+byte DIGOUT = 0; // value driven to Aux Dig Out, if DIGOUT_HiZ = 0, set to 0 for now  **
+
+// REG 4
+const byte WEAK_MISO = 1; // set to 1 from only one MISO line *; set to 0 to expand to 64 chan (2 module) system
+const byte TWOSCOMP = 0; // for unsigned ADC
+const byte ABSMODE = 0; // set to  0 for no absolute value function on ADC
+byte DSPen = 0; // 0 to disable offset removal for DSP
+byte DSP_cutoff = 0; // range = 0-15; does not matter if DSPen = 0
+
+// REG 5 **
+const byte Z_DAC_PWR = 0; // 0 for no impedance testing.
+const byte Z_LOAD = 0; // always 0 for normal operation
+const byte Z_SCALE = 0; // not important since no Z measurement done
+const byte Z_CONN_ALL = 0; //0 in normal operation
+const byte Z_SEL_POL = 0; // only used for for RHD2216 to select for sign
+const byte Z_EN = 0; // set to 0 for basic recovery
+
+// REG 6 **
+const byte Z_DAC = 0; // set to 0V
+
+// REG 7 **
+const byte Zcheck_SEL = 0 ; // set to 0 because Z_EN = 0
+
+// REG 14-17 (APWR)
+byte apwrRegisterAllOn = 0b11111111;
+
+
+
---
+++ TeensyFirmware_32chan/Intan_LV_interface_32chan/setSamplingPeriod.ino
@@ -0,0 +1,49 @@
+// ----------------------------------------
+// Set user defined sampling period
+//  01 Dec 2016, JE
+// ---------------------------------------
+
+
+const int computeSamplingPeriod() {
+
+  long SAMPLE_PERIOD_MICROS;
+
+// JE 12 dec 2016, not printing this in LV
+// SERIALNAME->println("Enter sampling period (per channel) in Hz");
+   
+  // print short string to acknowledge the event was received and we've entered the subfunction
+  // this is needed to help time LV and prevent race conditions
+   
+   //SERIALNAME->println("m_ack");
+   SERIALNAME->println("Input desired sampling frequency (Hz). Return string is actual Fs (Hz) ");
+   SERIALNAME->write(TERM_CHAR); // print termination character, to keep synced with LabView VISA R/W operations.
+                                 
+
+
+  // wait for user to input desired sampling rate
+  while (!SERIALNAME->available()) {
+    // wait for user input
+  }
+
+
+  int fs = SERIALNAME->parseInt();
+  serialFlush();
+
+  SAMPLE_PERIOD_MICROS = round(1000000 / fs);
+  float FS = 1000000/SAMPLE_PERIOD_MICROS; //sampling rate in Hz
+  
+  //SERIALNAME->print("Sampling period (us) set to = ");
+   //SERIALNAME->println(SAMPLE_PERIOD_MICROS);
+   //SERIALNAME->print("Actual Sampling Frequency (Hz) = ");
+   SERIALNAME->println(FS);
+   SERIALNAME->write(TERM_CHAR); // print termination character, to keep synced with LabView VISA R/W operations.
+  
+  
+  // SERIALNAME->print("Actual FS (Hz) = ");
+  // SERIALNAME->println(FS);
+  
+  
+  return SAMPLE_PERIOD_MICROS; //return sample period. used to set the Teensy Timer Interval
+
+}
+
